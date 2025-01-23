06153178cb4ee5b5928352da5bca5b876b397124
bmargerison <bmargerison@outlook.com>
2025-01-23 17:51:20 +0000

    scroll indicator and radio buttons

--- Startup3/script.js
+++ Startup3/script.js
@@ -1,12 +1,38 @@
 const carousel = document.querySelector('.hero__carousel');
 const cardWidth = carousel.querySelector('.hero__card').offsetWidth;
-const arrowBtns = document.querySelectorAll('.hero button')
+const radioBtns = document.querySelectorAll('.hero__buttons input');
+const leftArrow = document.querySelector('#left');
+const rightArrow = document.querySelector('#right');
 
-arrowBtns.forEach(btn => {
+leftArrow.addEventListener("click", () => {
+    for (var b = 0; b < radioBtns.length; ++b) {
+        if (radioBtns[b].checked == true) {
+            if (radioBtns[b].id.slice(-1) > 0) {
+                let newCheckedId = Number(radioBtns[b].id.slice(-1)) - 1;
+                document.querySelector(`#radio-${newCheckedId}`).checked = true;
+                carousel.scrollLeft -= cardWidth;
+                break;
+            }
+        }
+    }
+})
+
+rightArrow.addEventListener("click", () => {
+    for (var b = 0; b < radioBtns.length; ++b) {
+        if (radioBtns[b].checked == true) {
+            console.log(radioBtns[b]);
+            if (radioBtns[b].id.slice(-1) <= radioBtns.length) {
+                let newCheckedId = Number(radioBtns[b].id.slice(-1)) + 1;
+                document.querySelector(`#radio-${newCheckedId}`).checked = true;
+                carousel.scrollLeft += cardWidth;
+                break;
+            }
+        }
+    }
+})
+
+radioBtns.forEach(btn => {
     btn.addEventListener("click", () => {
-        console.log('clicked')
-        console.log(carousel)
-        console.log(cardWidth)
-        carousel.scrollLeft += btn.id === 'left' ?  -cardWidth : cardWidth
+        carousel.scrollLeft = cardWidth * (btn.id.slice(-1) - 1);
     });
 })
