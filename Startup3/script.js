1ee436cb40ce238ceb1168f7d84abd5fa6b8c407
bmargerison <bmargerison@outlook.com>
2025-01-30 19:01:35 +0000

    recalculate carousel width on window resize

--- Startup3/script.js
+++ Startup3/script.js
@@ -1,14 +1,22 @@
 const carousel = document.querySelector('.hero__carousel');
-const cardWidth = carousel.querySelector('.hero__card').offsetWidth;
+let cardWidth = carousel.querySelector('.hero__card').offsetWidth;
 const radioBtns = document.querySelectorAll('.hero__buttons input');
 const leftArrow = document.querySelector('#left');
 const rightArrow = document.querySelector('#right');
+let currentCard = 0;
+
+
+window.addEventListener("resize", (event) => {
+    cardWidth = carousel.querySelector('.hero__card').offsetWidth;
+    carousel.scrollLeft = `${currentCard * cardWidth}`;
+});
 
 leftArrow.addEventListener("click", () => {
     for (var b = 0; b < radioBtns.length; ++b) {
         if (radioBtns[b].checked == true) {
             if (radioBtns[b].id.slice(-1) > 0) {
                 let newCheckedId = Number(radioBtns[b].id.slice(-1)) - 1;
+                currentCard = newCheckedId - 1;
                 document.querySelector(`#radio-${newCheckedId}`).checked = true;
                 carousel.scrollLeft -= cardWidth;
                 break;
@@ -23,6 +31,7 @@ rightArrow.addEventListener("click", () => {
             console.log(radioBtns[b]);
             if (radioBtns[b].id.slice(-1) <= radioBtns.length) {
                 let newCheckedId = Number(radioBtns[b].id.slice(-1)) + 1;
+                currentCard = newCheckedId - 1;
                 document.querySelector(`#radio-${newCheckedId}`).checked = true;
                 carousel.scrollLeft += cardWidth;
                 break;
@@ -34,6 +43,6 @@ rightArrow.addEventListener("click", () => {
 radioBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         carousel.scrollLeft = cardWidth * (btn.id.slice(-1) - 1);
+        currentCard = btn.id.slice(-1) - 1;
     });
 })
-
